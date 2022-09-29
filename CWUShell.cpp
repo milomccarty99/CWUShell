78b448e9f37e345fd9d578d1a8fd7cf40813d909
milomccarty99 <milomccarty99@github.com>
2022-09-28 19:53:16 -0700

    implemented fileinfo and osinfo commands

--- CWUShell.cpp
+++ CWUShell.cpp
@@ -6,8 +6,8 @@
 #include <array>
 #include <memory>
 #include <stdexcept>
-#include <filesystem>
-#include <future>
+#include <fstream>
+//#include <future>
 //#include <bits/stdc++.h>
 
 using namespace std;
@@ -83,7 +83,7 @@ string* split(string str, char del, string* result)
 void cwushell_startup()
 {
 	system("clear");
-	cout<<"This is ashell for CS470 Operating Systems class" <<endl;
+	cout<<"This is a shell for CS470 Operating Systems class" <<endl;
 }
 
 // //eww
@@ -101,8 +101,9 @@ void cwushell_startup()
 // }
 
 // we exit the shell outputting the exit_code and using the standard exit with exit_code
-void exit_shell()
+void exit_shell(string* data)
 {
+
 	cout<< exit_code << endl;
 	std::exit(exit_code);
 }
@@ -128,8 +129,9 @@ int change_prompt(string* data) {
 
 	}
 }
-string get_switches(string* data)
+bool* get_switches(string* data, string switches_full, bool* active_switches)
 {
+	int active_switches_size = switches_full.size() + 1;
 	string switches = "";
 	for(int i = 1; i < data_token_len; i ++)
 	{
@@ -139,31 +141,60 @@ string get_switches(string* data)
 			{
 				switches+= data[i][j];
 			}
+			//cout<< data[i] << endl;
 		}
 	}
-	return switches;
-}
-int fileinfo_command_exec(string switches, string filename)
-{
-	cout<< "displaying info for "+ filename + " using switchs: " + switches << endl;
+	// convert the switches provided to a boolean array
+	for(int i = 0; i < switches.size(); i ++)
+	{
+		char sw = switches[i];
+		//cout << sw << " switch processing" << endl; 
+		bool sw_used = false;
+		for(int j = 0; j < switches_full.size(); j++)
+		{
+			if(sw == switches_full[j])
+			{
+				//cout << "using switch " << endl;
+				active_switches[j] = true; 
+				sw_used = true;
+			}
+
+		}
+		//improper switch used
+		if(!sw_used)
+		{
+			cout<< "Switch not recognized " << sw  << "" << endl;
+			active_switches[active_switches_size -1] = true;
+		}
+	}
 	
+	return active_switches;
+}
+int fileinfo_command_exec(bool* switches, string filename)
+{	
 	//-i from stat filename -c %i "inode"
 	//-t from stat filename -c %F
 	//-m from stat filename -c %y "
 
-	cout << "Inode number: ";
-	//wait();
-	string inode_querry = ("stat "+filename + " -c %i");
-	(system(inode_querry.c_str()));
-	
-	cout << "File type: ";
-	string filetype_querry = ("stat " + filename + " -c %F");
-	system(filetype_querry.c_str());
-
-	cout<< "Last modified: ";
-	string filemod_querry = ("stat " + filename + " -c %y");
-	system(filemod_querry.c_str());
-
+	if(switches[3])
+	{
+		cout << "improper switch usage" << endl;
+	}
+	if(switches[0])
+	{
+		string inode_querry = (" echo Inode Number:  `stat "+filename + " -c %i`");
+		(system(inode_querry.c_str()));
+	}
+	if(switches[1])
+	{
+		string filetype_querry = ("echo File type: `stat " + filename + " -c %F`");
+		system(filetype_querry.c_str());
+	}
+	if (switches[2])
+	{
+		string filemod_querry = ("echo Last modified: `stat " + filename + " -c %y`");
+		system(filemod_querry.c_str());
+	}
 	return 0;
 }
 //fileinfo -i -t -m  filename 
@@ -171,7 +202,8 @@ int fileinfo_command_exec(string switches, string filename)
 int fileinfo_command(string* data)
 {
 	string filename;
-	string cmd_switches = get_switches(data);
+	bool cmd_switches[4] = {0};
+	get_switches(data, "itm", cmd_switches);
 	for(int i = 1; i < data_token_len; i++)
 	{
 		if(data[i][0] != '-')
@@ -187,10 +219,54 @@ int fileinfo_command(string* data)
 			}
 		}
 	}
+	fstream file_stream;
+	file_stream.open(filename);
+	if (file_stream.fail())
+	{
+		file_stream.close();
+		cout<<"File does not exist." << endl;;
+		return 0;
+	}
 	return fileinfo_command_exec(cmd_switches,filename);
 }
 //osinfo -T -b -s (or type of file system, number of blocks in the file system, maximum number of characters in a file name)
-
+int osinfo_command(string* data)
+{
+	bool switches[4] = {0};
+	get_switches(data,"Tbs", switches);
+	for(int i = 1; i < data_token_len; i++)
+	{
+		if(data[i][0]!= '-')
+		{
+			cout<<"unexpected token " + data[i] << endl;
+			return 2;
+		}
+	}
+	if(switches[3])
+	{
+		cout << "improper switch usage" << endl;
+		return 0;
+	}
+	//T
+	if(switches[0])
+	{
+		string filesystem_querry = "echo File system type: `df -Th / | grep '/dev' | awk '{print$2}'`";
+		system(filesystem_querry.c_str());
+	}
+	//b
+	if(switches[1])
+	{
+		string numblocksystem_querry = "echo Number of blocks: `df / | grep '/dev' | awk '{print$2}'`";
+		system(numblocksystem_querry.c_str());
+	}
+	//s
+	if(switches[2])
+	{
+		string filenamemaxchar_querry = "echo Max characters in file name: `getconf -a | grep -m1 'NAME_MAX' | awk '{print$2}'`";
+		system(filenamemaxchar_querry.c_str());
+	}
+	return 0;
+}
 //all other shell commands
 int shell_command(string cmd)
 {
@@ -212,7 +288,7 @@ int command_switch(string* data, string raw_input)
 	switch(command)
 	{
 		case Commands::exit_shell_command:
-			exit_shell();
+			exit_shell(data);
 			break;
 		case Commands::prompt:
 			return change_prompt(data);
@@ -221,8 +297,8 @@ int command_switch(string* data, string raw_input)
 			return fileinfo_command(data);
 			break;
 		case Commands::osinfo:
-		//bloc
-		break;
+			return osinfo_command(data);
+			break;
 		case Commands::system_command:
 			return shell_command(raw_input);
 			break;
